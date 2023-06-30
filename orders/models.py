964611ab6e49728c594689b213c239d653f89e37
GustavoCruz <gustavo0201cruz@gmail.com>
2023-06-30 14:53:15 -0400

    feat: model order and serializer

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class StatusChoices(models.TextChoices):
+    CONFIRMED = "confirmed"
+    IN_PROGRESS = "in_progress"
+    DELIVERED = "delivered"
+
+
+class Order(models.Model):
+    status = models.CharField(choices=StatusChoices.choices, default=StatusChoices.IN_PROGRESS)
+    product_quantity = models.IntegerField()
+    created_at = models.DateTimeField()
