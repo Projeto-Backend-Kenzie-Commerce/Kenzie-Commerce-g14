152bfae0162b074287bb6a022fcf10345b99366d
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-06-30 16:08:48 -0300

    create user and adress model/serializer

--- address/models.py
+++ address/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    class Meta:
+        ordering = ["id"]
+
+    street = models.CharField(max_length=100)
+    number = models.IntegerField()
+    city = models.CharField(max_length=30)
+    block = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.PROTECT,
+        related_name="address",
+    )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, unique=True)
+    is_employee = models.BooleanField(null=True, default=False)
