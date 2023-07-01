b538409727f65b3d55ae697c7a316f4f49f53148
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-07-01 16:41:33 -0300

    hotfix: is_admin model user and requirements updated

--- users/models.py
+++ users/models.py
@@ -7,3 +7,4 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100, unique=True)
     is_employee = models.BooleanField(null=True, default=False)
+    is_admin = models.BooleanField(default=False)
--- users/serializers.py
+++ users/serializers.py
@@ -14,6 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "is_employee",
+            "is_admin",
             "is_active",
         ]
         read_only_fields = ["id"]
