e85f1b4c8667bf4207486fcca3b0e8387e652a5c
Alysson Fernandes <alyss0n_F@hotmail.com>
2023-07-03 16:35:48 -0300

    feat: database change to POSTGRES, routes LIST all Products and CREATE Products created

--- products/views.py
+++ products/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class ProductView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+
+# class UserDetailView(RetrieveUpdateDestroyAPIView):
+#     authentication_classes = [JWTAuthentication]
+#     # permission_classes = [IsAccountOwner]
+
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     lookup_url_kwarg = "pk"
