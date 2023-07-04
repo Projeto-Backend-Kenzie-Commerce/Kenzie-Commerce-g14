c0b49c3ce6f9a81f599addd088c94841a645a97c
GustavoCruz <gustavo0201cruz@gmail.com>
2023-07-04 12:21:05 -0400

    fix: changes

--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import Product
 
+
 class ProductSerializer(serializers.ModelSerializer):
     # user = UserSerializer(read_only=True)
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'description', 'stock_quantity']
+        fields = ["id", "name", "price", "description", "stock_quantity"]
