6841da9821e43773b872bcf4181c03108ea7e289
GustavoCruz <gustavo0201cruz@gmail.com>
2023-07-06 13:29:30 -0400

    feat: views finished

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
-from .models import StatusChoices, Order
+from .models import Order
 from django.shortcuts import get_object_or_404
 from products.models import Product
+from rest_framework.exceptions import ValidationError
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -21,13 +22,25 @@ class OrderSerializer(serializers.ModelSerializer):
     def create(self, validated_data: dict) -> Order:
         validated_data.pop("user")
         validated_data.pop("is_employee")
+        quantity = validated_data.get("product_quantity")
         data_products = validated_data.pop("product", [])
 
         products = []
         for product in data_products:
             product = get_object_or_404(Product, id=product.id)
+            if product.stock_quantity < quantity:
+                raise ValidationError(
+                    "quantity ordered is greater than quantity in stock"
+                )
             products.append(product)
 
         order = Order.objects.create(**validated_data)
         order.product.set(products)
         return order
+
+    def update(self, instance: Order, validated_data: dict) -> Order:
+        status = validated_data.get("status")
+        if status:
+            instance.status = status
+            instance.save()
+        return instance
