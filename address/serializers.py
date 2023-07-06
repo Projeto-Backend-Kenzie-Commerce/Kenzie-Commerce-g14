333a090d3f3f630d996be61702b4a67eaa43fef0
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-07-06 15:43:29 -0300

    feat: create address rules

--- address/serializers.py
+++ address/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
 from address.models import Address
 
@@ -27,25 +28,56 @@ class AddressSerializer(serializers.ModelSerializer):
     def get_email(self, obj):
         return obj.user.email
 
+    def validate(self, attrs: dict) -> dict:
+        keys_exists = "is_default" in attrs
+        if not keys_exists:
+            attrs["is_default"] = True
+
+        is_default = attrs.get("is_default", None)
+
+        if is_default:
+            user_id = self.context["request"].user.id
+            has_default_address = Address.objects.filter(
+                user_id=user_id, is_default=True
+            ).first()
+
+            if has_default_address:
+                has_default_address.is_default = False
+                has_default_address.save()
+        else:
+            user_id = self.context["request"].user.id
+            if self.instance:
+                address_id = self.instance.id
+
+                has_default_address = Address.objects.filter(
+                    user_id=user_id, is_default=True
+                ).first()
+
+                if not has_default_address:
+                    raise serializers.ValidationError(
+                        {
+                            "message": "Pelo menos um endereço deve ser definido como padrão."
+                        }
+                    )
+                if address_id == has_default_address.id:
+                    raise serializers.ValidationError(
+                        {
+                            "message": "Pelo menos um endereço deve ser definido como padrão."
+                        }
+                    )
+            has_default_address = Address.objects.filter(
+                user_id=user_id, is_default=True
+            ).exists()
+
+            if not has_default_address:
+                raise serializers.ValidationError(
+                    {"message": "Pelo menos um endereço deve ser definido como padrão."}
+                )
+
+        return attrs
+
     def create(self, validated_data: dict) -> Address:
         return Address.objects.create(**validated_data)
 
-    def update(self, instance: Address, validated_data: dict) -> Address:
-        user_id = self.context["view"].kwargs["pk"]
 
-        has_default_address = Address.objects.filter(
-            user_id=user_id, is_default=True
-        ).exists()
-
-        if has_default_address:
-            Address.objects.filter(user_id=user_id).update(is_default=False)
-
-        if "is_default" in validated_data and validated_data["is_default"]:
-            validated_data["is_default"] = not has_default_address
-
-        return super().update(instance, validated_data)
-
-
-#  para fazer a lógica do default eu vou usar o update e fazer o segunte
-#  verificar se existe o defult true, então antes de ele atualizar eu coloco todos como false,
-# e deixo o novo que foi adicionado/atualizado como true depois da att.
+#  fazer as rotas no insomnia com employee e sem employee com admin e sem admin
--- address/urls.py
+++ address/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from .views import AddressView, AddressDetailView
 
 
 urlpatterns = [
-    path("users/<int:pk>/address/", AddressView.as_view()),
-    path("users/<int:pk>/address/<int:id>/", AddressDetailView.as_view()),
+    path("address/", AddressView.as_view()),
+    path("address/<int:id>/", AddressDetailView.as_view()),
 ]
